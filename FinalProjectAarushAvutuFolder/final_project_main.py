c625f0464f48241d972bb990a0df8ed71f80777d
Aarush A <95145876+AarushAvutu@users.noreply.github.com>
2023-12-01 12:55:03 -0800

    Add files via upload

---
+++ FinalProjectAarushAvutuFolder/final_project_main.py
@@ -0,0 +1,26 @@
+''' Task schedular
+after set amount of time does task
+ex: delete unused tabs
+running program
+backups
+Maintenance: disk clear up trash empty
+sending automated notifications (calender maybe)
+automation w/ smarthome devices (turn lights on and off at time when come home)
+updates (checks for needed app updates and updates at night)
+timer to remind yourself something
+notifications on and off toggle for specifics
+
+or
+
+Multiplayer game
+3D
+objective: find treasure
+platformer
+powerups
+mobs?
+everytime finds treasure at level theif comes and takes to next lvl?
+last lvl defeat theif
+
+
+
+'''
